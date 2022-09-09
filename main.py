5f002e3d14044f7f73e0fb35355ffe3f493d204b
Krauchanka Aliaksandr <krav1983@tut.by>
2022-09-09 08:36:04 +0300

    Сапер

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-import sys
+from random import randint
 
-class ListObject():
-    def __init__(self, data, next_obj = None):
-        self.data = data
-        self.next_obj = next_obj
+class Cell():
+    def __init__(self, around_mines=0, mine=False):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = False
 
-    def link(self, obj):
-        self.next_obj = obj
 
-lst_in = list(map(str.strip, sys.stdin.readlines()))
+class GamePole():
+    def __init__(self, N ,M):
+        self._n = N
+        self._m = M
+        self.pole = [[Cell() for _ in range(self._n)] for _ in range(self._n)]
+        self.init()
 
-head_obj = ListObject(lst_in[0])
-obj = head_obj
-for i in range(1, len(lst_in)):
-    obj_new = ListObject(lst_in[i])
-    obj.link(obj_new)
-    obj = obj_new
+    def init(self):
+        m = 0
+        while m < self._m:
+            i = randint(0, self._n - 1)
+            j = randint(0, self._n - 1)
+            if self.pole[i][j].mine:
+                continue
+            self.pole[i][j].mine = True
+            m += 1
+        indx = (-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
+
+        for x in range(self._n):
+            for y in range(self._n):
+                if not self.pole[x][y].mine:
+                    mines = sum((self.pole[x+i][y+j].mine for i, j in indx if 0 <= x+i < self._n and 0 <= y+j < self._n))
+                    self.pole[x][y].around_mines = mines
+
+    def show(self):
+        for row in self.pole:
+            print(*map(lambda x: '#' if not x.fl_open else x.around_mines if not x.mine else '*', row))
+
+pole_game = GamePole(10, 12)
