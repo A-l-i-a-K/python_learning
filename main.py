f529af00bdfacb567be1a746eb839fd6e69e2100
Krauchanka Aliaksandr <krav1983@tut.by>
2022-09-09 08:36:56 +0300

    Корзина

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
-from random import randint
+class Cart():
+    def __init__(self):
+        self.goods = []
 
-class Cell():
-    def __init__(self, around_mines=0, mine=False):
-        self.around_mines = around_mines
-        self.mine = mine
-        self.fl_open = False
+    def add(self, gd):
+        self.goods.append(gd)
+
+    def remove(self, indx):
+        del self.goods[indx]
+
+    def get_list(self):
+        return [f'{el.name}: {el.price}' for el in self.goods]
+
+class Table():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 
-class GamePole():
-    def __init__(self, N ,M):
-        self._n = N
-        self._m = M
-        self.pole = [[Cell() for _ in range(self._n)] for _ in range(self._n)]
-        self.init()
+class TV():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-    def init(self):
-        m = 0
-        while m < self._m:
-            i = randint(0, self._n - 1)
-            j = randint(0, self._n - 1)
-            if self.pole[i][j].mine:
-                continue
-            self.pole[i][j].mine = True
-            m += 1
-        indx = (-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
 
-        for x in range(self._n):
-            for y in range(self._n):
-                if not self.pole[x][y].mine:
-                    mines = sum((self.pole[x+i][y+j].mine for i, j in indx if 0 <= x+i < self._n and 0 <= y+j < self._n))
-                    self.pole[x][y].around_mines = mines
+class Notebook():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-    def show(self):
-        for row in self.pole:
-            print(*map(lambda x: '#' if not x.fl_open else x.around_mines if not x.mine else '*', row))
 
-pole_game = GamePole(10, 12)
+class Cup():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+cart = Cart()
+cart.add(TV('Samsung', 1700))
+cart.add(TV('Philips', 1500))
+cart.add(Table('AMI', 300))
+cart.add(Notebook('Lenovo', 1700))
+cart.add(Cup('AMI', 10))
+
