80466e52d8e309ccfda813401213a256afe4d9fd
Krauchanka Aliaksandr <krav1983@tut.by>
2022-09-13 06:08:06 +0300

    Viber

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
-class AppStore ():
-    def __init__(self):
-        self.apps = {}
+class Viber():
 
-    def add_application(self, app):
-        self.apps[id(app)] = app
+    mess = {}
 
-    def remove_application(self, app):
-        self.apps.pop(id(app))
+    @classmethod
+    def add_message(cls, msg):
+        cls.mess[id(msg)] = msg
 
-    def block_application(self, app):
-        obj = self.apps.get(id(app), False)
-        if not obj:
-            return False
-        obj.blocked = True
-        return True
+    @classmethod
+    def remove_message(cls, msg):
+        if id(msg) in cls.mess:
+            cls.mess.pop(id(msg))
+
+    @classmethod
+    def set_like(cls, msg):
+        if id(msg) in cls.mess:
+            msg.fl_like = not msg.fl_like
 
 
-    def total_apps(self):
-        return len(self.apps)
+    @classmethod
+    def show_last_message(cls, num):
+        for m in tuple(cls.mess.values())[-num:]:
+            print(m)
 
-class Application():
-    def __init__(self, name, blocked=False):
-        self.name = name
-        self.blocked = blocked
+    @classmethod
+    def total_messages(cls):
+        return len(cls.mess)
 
-store = AppStore()
-app_youtube = Application("Youtube")
-store.add_application(app_youtube)
-store.remove_application(app_youtube)
+class Message():
+    def __init__(self, text, fl_like=False):
+        self.text = text
+        self.fl_like = fl_like
+
+msg = Message("Всем привет!")
+Viber.add_message(msg)
+Viber.add_message(Message("Это курс по Python ООП."))
+Viber.add_message(Message("Что вы о нем думаете?"))
+Viber.add_message(Message("1Что вы о нем думаете?"))
+Viber.add_message(Message("2Что вы о нем думаете?"))
+Viber.add_message(Message("3Что вы о нем думаете?"))
+Viber.set_like(msg)
+Viber.remove_message(msg)
+
+
+print(Viber.show_last_message(1))
+print(Viber.total_messages())
+
+
